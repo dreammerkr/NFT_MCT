766e4afb42e6b7ac5e022854ba4e8780d5b3baea
NeoMRN <sumathi@snu.ac.kr>
2021-04-08 21:38:27 +0900

    SHK - modified generation and card adding.

--- contracts/MCT.sol
+++ contracts/MCT.sol
@@ -9,44 +9,35 @@ contract MCT is ERC721URIStorage{
     event MCTInit();
     event NewGen();
     event SoldCard(uint _id);
-    
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint gatchaprice;
+    uint lastId;
+    uint[] numOfCardsPerGen;
+    uint currGen;//starts from 1, not 0
     
     address public admin;
     constructor() ERC721('MCT', 'Card'){
         admin=msg.sender;
         lastId=0;
-        currGen=0;
+        currGen=0; //this will be incremented as soon as startNewGen() is called.
         MCTInit();
     }
-    /*
-    function balanceOf(address _owner) public override view returns (uint256){
-        
-    }
-    function ownerOf(uint256 _tokenId) public override view returns (address){
-        
-    }
-    function transferFrom(address _from, address _to, uint256 _tokenId) public override {
-        
-    }
-    function approve(address _approved, uint256 _tokenId) public override {
-        
-    }*/
-    
+
 
     
-    uint gatchaprice;
-    uint lastId;
-    uint[] numOfCardsPerGen;
-    uint currGen;
-    
 
-    function generatePool(uint _numofnewcards /*What to input for URI's? array of strings are not available in Solidity (yet)*/) external {
+    function addCard(string memory _uri) external {
         require(msg.sender==admin);
-        for(uint i=1; i<=_numofnewcards; i++){
-            _setTokenURI(lastId+i, ""); //How to pass strings in a nested loop? Maybe just call it everytime?
-        }
-        numOfCardsPerGen[++currGen]=_numofnewcards;
-        lastId+=_numofnewcards;
+        numOfCardsPerGen[currGen-1]++;
+        _tokenIds.increment();
+        uint id=_tokenIds.current();
+        _setTokenURI(id, _uri);
+    }
+    
+    function startNewGen() external{
+        numOfCardsPerGen.push(0);
+        currGen++;
         NewGen();
     }
     
@@ -63,8 +54,6 @@ contract MCT is ERC721URIStorage{
         SoldCard(id);
     }
     
-    //checking github remote
-
     function _mintCard(address _to, uint _id) private {
         _safeMint(_to, _id);
         

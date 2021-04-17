b4ed39401e74f512053dc03e2067fca3966ebbc4
Kwak Taewon <tykwak94@gmail.com>
2021-04-17 22:34:22 +0900

    edit config.js file

--- truffle-config.js
+++ truffle-config.js
@@ -18,6 +18,12 @@
  *
  */
 
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
@@ -58,9 +64,9 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
-	    provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/a48cec8bf2f2401a8f0e6b934a42e53c`),
-    network_id: 3,       // Ropsten's id
+    // ropsten: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+    // network_id: 3,       // Ropsten's id
     // gas: 5500000,        // Ropsten has a lower block limit than mainnet
     // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
@@ -72,6 +78,21 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/a48cec8bf2f2401a8f0e6b934a42e53c`),
+      network_id: 3,       // Ropsten's id
+      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      // },
+      // Useful for private networks
+      // private: {
+      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+      // network_id: 2111,   // This network is yours, in the cloud.
+      // production: true    // Treats this network as if it was a public net. (default: false)
+      // }
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -82,8 +103,8 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0"    // Fetch exact version from solc-bin (default: truffle's version)
-      //docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
       //    enabled: false,
@@ -103,5 +124,4 @@ module.exports = {
   db: {
     enabled: false
   }
-  }
-}
+};
